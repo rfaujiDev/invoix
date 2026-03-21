b80925a1f948be8aab5e7e38e74bdb753efb87e0
rfaujiDev <mrizkifauji.rizki@gmail.com>
2026-03-21 14:43:42 +0800

    Invoix showcase root

---
+++ fix-names.sh
@@ -0,0 +1,4 @@
+sed -i "s|images/_home_rfauji_invoice-app_renderer_index.html.png|images/_home_rfauji_invoice-app_renderer_index.html.png|g" index.html
+sed -i "s|Screenshot_2026-03-21_00-02-18_template pdf 1.png|Screenshot_2026-03-21_00-02-18_template_pdf_1.png|g" index.html
+sed -i "s|Screenshot_2026-03-21_00-03-21_template pdf 2.png|Screenshot_2026-03-21_00-03-21_template_pdf_2.png|g" index.html
+sed -i "s|Screenshot_2026-03-21_00-04-06_template pdf 3.png|Screenshot_2026-03-21_00-04-06_template_pdf_3.png|g" index.html
